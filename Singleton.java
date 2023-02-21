bdb8cb3972e1194bb0863865820e2e59efb63b9f
Abdul-Adhil <125632131+Abdul-Adhil@users.noreply.github.com>
2023-02-21 13:06:26 +0530

    Singleton Program added

---
+++ Singleton.java
@@ -0,0 +1,31 @@
+public class Singleton {
+    public static void main(String[] args) {
+        SingletonClass si = SingletonClass.getInstance();
+        SingletonClass si2 = SingletonClass.getInstance();
+
+        System.out.println(si);
+        System.out.println(si2);
+
+    }
+}
+
+class SingletonClass {
+
+    private static SingletonClass singletonclass = null;
+
+    private SingletonClass() { // constructor
+
+        System.out.println("Singleton object is created");
+    };
+
+    public static SingletonClass getInstance() { // factory method
+        if (singletonclass == null) {
+            singletonclass = new SingletonClass();
+            return singletonclass;
+        } else {
+            return singletonclass;
+        }
+
+    }
+
+}
