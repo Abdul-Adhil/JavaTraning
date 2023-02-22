c953085e201513c9aaf91a5b10735fd35daf9f7c
Abdul-Adhil <125632131+Abdul-Adhil@users.noreply.github.com>
2023-02-22 19:51:00 +0530

    Primitive Program added

--- Primitive.java
+++ Primitive.java
@@ -4,7 +4,6 @@ public class Primitive {
     static long p2;
     static float p3;
     static double p4;
-    static String p5;
     static boolean p6;
     static short p7;
     static byte p8;
@@ -15,7 +14,7 @@ public class Primitive {
         System.out.println(p2);
         System.out.println(p3);
         System.out.println(p4);
-        System.out.println(p5);
+        // System.out.println(p5);
         System.out.println(p6);
         System.out.println(p7);
         System.out.println(p8);
