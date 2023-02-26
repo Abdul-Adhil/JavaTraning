dc8391113bd6f403f298d2ffb75a7adb8015ad91
Abdul-Adhil <125632131+Abdul-Adhil@users.noreply.github.com>
2023-02-26 15:48:13 +0530

    program added

--- P4_DigitalClock.java
+++ P4_DigitalClock.java
@@ -40,9 +40,14 @@ class DisplayTime {
     private int hour;
     private int minute;
     private int second;
+    private String formate;
 
     public DisplayTime(int hour, int minute, int second) {
-        this.hour = hour;
+        if (hour >= 12) {
+            this.hour = hour - 12;
+            this.formate = "PM";
+        }
+        // this.hour = hour;
         this.minute = minute;
         this.second = second;
 
@@ -50,27 +55,99 @@ class DisplayTime {
 
     synchronized public void hour() {
 
-    }
+        if (minute == 59) {
+            minute = 0;
+            second = 0;
+            hour++;
+            if (hour > 12) {
+                hour = 1;
+                this.formate = "AM";
+            }
 
-    synchronized public void minute() {
-    }
+            try {
 
-    synchronized public void second() {
+                Thread.sleep(1000);
 
-        if (second == 60) {
+            } catch (Exception e) {
+
+            }
+
+            notifyAll();
+
+        } else {
+
             try {
                 wait();
 
             } catch (Exception e) {
 
             }
+
         }
-        for (int i = second; i < 60; i++) {
-            System.out.println(hour + ":" + minute + ":" + second);
-            second += 1;
+
+    }
+
+    synchronized public void minute() {
+
+        if (second == 60 && minute < 59) {
+
+            second = 0;
+            minute++;
+
+            try {
+
+                Thread.sleep(1000);
+
+            } catch (Exception e) {
+
+            }
+
+            notifyAll();
+
+        } else {
+
+            try {
+                wait();
+
+            } catch (Exception e) {
+
+            }
+
         }
-        second = 60;
-        notifyAll();
+
+    }
+
+    synchronized public void second() {
+
+        int i;
+
+        if (second < 60) {
+
+            for (i = second; i < 60; i++) {
+                try {
+
+                    Thread.sleep(1000);
+
+                } catch (Exception e) {
+
+                }
+
+                System.out.println(hour + ":" + minute + ":" + i + " " + formate);
+            }
+
+            second = i;
+            notifyAll();
+
+        } else {
+
+            try {
+                wait();
+
+            } catch (Exception e) {
+
+            }
+
+        }
 
     }
 
