e5cfdc73d992ddc337a237c74ee763e3a479a741
Abdul-Adhil <125632131+Abdul-Adhil@users.noreply.github.com>
2023-02-26 22:12:32 +0530

    program added

--- P6_Alphabet.java
+++ P6_Alphabet.java
@@ -1,10 +1,56 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.Scanner;
+
 public class P6_Alphabet {
     public static void main(String[] args) {
+
+        ExecutorService es = Executors.newFixedThreadPool(1);
+        Alph alphabetCount = new Alph();
+
+        es.execute(() -> {
+
+            while (true) {
+                alphabetCount.countAlphabet();
+            }
+        });
+        es.shutdown();
+    }
+
+}
+
+class Alph {
+    int count = 0;
+
+    void countAlphabet() {
+
+        System.out.println("Enter a Character: ");
+        Scanner scan = new Scanner(System.in);
+        char c = (char) scan.next().charAt(0);
+
+        try {
+            if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+                count++;
+            else
+                throw new NotLetterException();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Total Count: " + count);
+
+            System.exit(0);
+
+        }
 
     }
 
 }
 
-class Alphabet {
+class NotLetterException extends Exception {
+
+    @Override
+    public String toString() {
+        return "Not a Letter Exception";
+    }
 
 }
--- P8.java
+++ P8.java
@@ -1,3 +1,51 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class P8 {
+    public static void main(String[] args) {
+
+        ExecutorService es = Executors.newFixedThreadPool(3);
+        StringBuffer sbObj = new StringBuffer("A");
+        MyThread objBuff = new MyThread(sbObj);
+
+        es.execute(() -> {
+            objBuff.run();
+        });
+        es.execute(() -> {
+            objBuff.run();
+        });
+        es.execute(() -> {
+            objBuff.run();
+        });
+        es.shutdown();
+
+    }
+
+}
+
+class MyThread extends Thread {
+
+    StringBuffer buffer;
+
+    MyThread(StringBuffer buffer) {
+        this.buffer = buffer;
+    }
+
+    @Override
+    public void run() {
+
+        synchronized (buffer) {
+
+            for (int i = 0; i < 100; i++) {
+                System.out.print(buffer + " ");
+            }
+            System.out.println();
+            char nextChar = buffer.charAt(0);
+            nextChar++;
+            buffer = buffer.replace(0, 1, String.valueOf(nextChar));
+
+        }
+
+    }
 
 }
